64286d76c80a89fa05445acf8745735d882c5997
maxim15679 <maxim15679@mail.ru>
2016-11-11 00:20:45 +0300

    Add files via upload

--- canonize.js
+++ canonize.js
@@ -1,8 +1,8 @@
 export default function canonize(url) {
-  const re = new RegExp('(https?:)?(\/\/)?([A-Za-z0-9]*.([A-Za-z0-9]*)\/)?(@)?([A-Za-z0-9]*)');
+  const re = new RegExp('(https?:)?(\/\/)?(www.)?([A-Za-z0-9-]*.([A-Za-z0-9-]*)\/)?(@)?([A-Za-z0-9._]*)');
   const match = url.match(re);
-  if (match[6]){
-    return match[6];
+  if (match[7]){
+    return match[7];
   }else {
     return false;
   }
